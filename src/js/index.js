db1259479b5a23e13b02f4f9943ea8c6d052f0ed
pozchaa <igorleonsob@icloud.com>
2023-02-12 17:55:07 +0300

    изменение арх стилей

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+import './anchor-slow'
+import './card'
+import './filter'
+import './input'
+import './menu'
+import './swiper-home'
+
+import '../styles/main-css/styles.scss'
+import '../styles/main-css/header.scss'
+import '../styles/main-css/bonus.scss'
+import '../styles/main-css/card.scss'
+import '../styles/main-css/footer.scss'
+import '../styles/main-css/filter.scss'
+import '../styles/main-css/main.scss'
+import '../styles/main-css/menu.scss'
+import '../styles/main-css/rules.scss'
+import '../styles/main-css/svg-animation.scss'
+
+import '../styles/media-css/mobile-card.scss'
+import '../styles/media-css/mobile-filter.scss'
+import '../styles/media-css/mobile-swiper.scss'
+import '../styles/media-css/tablet-card.scss'
+import '../styles/media-css/tablet-footer.scss'
+import '../styles/media-css/tablet-header.scss'
+import '../styles/media-css/tablet-main.scss'
+import '../styles/media-css/tablet-styles.scss'
+import '../styles/media-css/tablet-swiper.scss'
+// import '../blocks/main/main.scss';
+// import '../blocks/feedback/feedback.scss';
+// import '../blocks/service-brands/service-brands.scss';
+// import '../blocks/service-technic/service-technic.scss';
+// import '../blocks/menu/menu.scss';
+// import '../blocks/price/price.scss';
+// import '../blocks/footer/footer.scss';
+// import '../blocks/media/desktop.scss';
+// import '../blocks/media/tablet.scss';
+// import '../blocks/swiper/swiper.scss';
